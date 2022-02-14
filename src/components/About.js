97ac8136b069e3a86da342f78ce11c4f0714a250
Ravinder <74204725+ravinderkumar72857@users.noreply.github.com>
2022-02-14 20:30:58 +0530

    deployed

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,22 @@
 import React from 'react'
 
 export const About = () => {
+  let myStyle={
+    minHeight:"90vh"
+  }
   return (
-    <div>
-        <h1 className='text-center'>Ravinder</h1>
-        <hr/>
-        <p>A full stack web developer</p>
+    
+        <div class="card text-end" style="width: 18rem;" style={myStyle}>
+  <div class="card-body">
+  
+    <h5 class="card-title">contact us</h5>
+   <ul class="card-text list-unstyled text-info" >
+     <li>9467193184</li>
+     <li>9817762377</li>
+     <li>ravinderkumar72857@gmail.com</li>
+   </ul>
+  </div>
+
     </div>
   )
 }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,12 +1,20 @@
 import React from 'react'
 
 export const TodoItem = ({todo,onDelete}) => {
+  let cStyle={
+    width: "20rem"
+  }
   return (
-    <div>
+    
       
-      <h4>{todo.title}</h4>
-      <p>{todo.desc}</p>
-      <button className="btn btn-danger btn-sm" onClick={()=>{onDelete(todo)}}>X</button>
+    <div className="card   shadow p-3 mb-5 bg-white rounded" style={cStyle}>
+    <div className="card-body">
+      <h4 className="card-title">{todo.title}</h4>
+      <p className="card-text text-success">{todo.desc}</p>
+      <button className="btn btn-danger btn-sm" onClick={()=>{onDelete(todo)}}>Delete</button>
+    
+      
     </div>
+  </div>
   )
 }
